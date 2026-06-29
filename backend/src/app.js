75eb4af353dec9c75976b214b83ff5573d6671fb
Rimjhim Kumari <rimjhim.3279@gmail.com>
2026-06-29 17:52:24 +0530

    feat: setup professional express backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,19 +2,25 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import notFound from "./middleware/notFound.js";
+import errorHandler from "./middleware/errorHandler.js";
 
+import healthRoutes from "./routes/health.routes.js";
 
 const app = express();
 
 app.use(cors());
+
 app.use(helmet());
+
 app.use(morgan("dev"));
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "AI Driver Monitoring Backend Running"
-    });
-});
+app.use("/api/health", healthRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
-import app from "./app.js";
 
 dotenv.config();
 
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
